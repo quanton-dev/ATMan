25fd1822d897884df8c9c8b7aae338c451615aec
K0FT3 <yusufgndz99@gmail.com>
2020-05-28 20:20:27 +0300

    fixed build and lint problems

--- front-end/tests/e2e/specs/login.e2e..js
+++ front-end/tests/e2e/specs/login.e2e..js
@@ -2,11 +2,11 @@
 // https://nightwatchjs.org/guide
 
 module.exports = {
-  "login test": function(browser) {
+  'login test': function(browser) {
     browser
-      .url(process.env.VUE_DEV_SERVER_URL + "login")
-      .waitForElementVisible("#app", 5000)
-      .assert.containsText("h1", "ATMan")
-      .end();
+      .url(process.env.VUE_DEV_SERVER_URL + 'login')
+      .waitForElementVisible('#app', 5000)
+      .assert.containsText('h1', 'ATMan')
+      .end()
   }
-};
+}
