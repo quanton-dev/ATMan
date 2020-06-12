392d9a9a62be69cc34853d5b6aa8a142961bcafb
K0FT3 <yusufgndz99@gmail.com>
2020-06-12 13:35:51 +0300

    typo

--- docker/start.sh
+++ docker/start.sh
@@ -1,5 +1,5 @@
-docker pull taskagile/vuejs.spring-boot.mysql:$1
-docker container stop taskagile
-docker run --detach --rm --name taskagile --env-file ./env.list \
+docker pull atman/vuejs.spring-boot.mysql:$1
+docker container stop atman
+docker run --detach --rm --name atman --env-file ./env.list \
   -e "SPRING_PROFILES_ACTIVE=staging,docker" \
-  -p 8080:8080 -p 9000:9000 taskagile/vuejs.spring-boot.mysql:$1
+  -p 8080:8080 -p 9000:9000 atman/vuejs.spring-boot.mysql:$1
